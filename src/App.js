c6830adf925c3198193f3d10445eee16b074c48b
samyprdhn <samyprdhn@gmail.com>
2023-03-09 18:23:57 -0500

    feat: project init

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import moon from './moon.svg';
 import './App.css';
 
 function App() {
@@ -6,18 +7,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <footer className="App-footer">
+        <img src={moon} className="App-logo" alt="moon" />
+      </footer>
     </div>
   );
 }
